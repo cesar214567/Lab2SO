dba60833807d215c99311a18fabe50cd94c5aa2e
StephanoWurttele <stephano.wurttele@utec.edu.pe>
2020-11-04 19:07:25 -0500

    Improvements

--- barberos/barberosv2.c
+++ barberos/barberosv2.c
@@ -1,18 +1,17 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <semaphore.h>
 #define N_Barbers 3
-#define N_Persons 10
+#define N_Persons 100
 #define SEATS 5
 
 
 int waiting_list = 0;
-int num_personas = N_Persons; // missing people
 sem_t customers; //numbers of waiters
-sem_t barberos;  // free barbers
+sem_t barberos;  //free barbers
+sem_t num_personas; //people left
 
 pthread_mutex_t lock_seats; //to block person actions 
 pthread_mutex_t personas;   //to block decreasing num_personas counter
@@ -22,15 +21,15 @@ pthread_t persons[N_Persons];
 
 void *barber(void* id){
     int ID = (int) id;
-    while (num_personas) {
+    while (sem_trywait(&num_personas) != -1) {
         sem_post(&barberos); /* one barber is now ready to cut hair */
         sem_wait(&customers); /* go to sleep if # of customers is 0 */
         pthread_mutex_lock(&lock_seats); /* acquire access to "waiting' */
         waiting_list--; /* decrement count of waiting customers */
         pthread_mutex_unlock(&lock_seats); /* release 'waiting' */
         printf("Cortando cabello, barber id: %d\n", ID); /* cut hair (outside critical region */
-    }
-    printf("FIN DEL DIA\n");
+  }
+    printf("FIN DEL DIA %d\n", temp);
 }
 void *person(void* id){
     int ID = (int) id;
@@ -41,22 +40,11 @@ void *person(void* id){
         sem_wait(&barberos); /* go to sleep if # of free barbers is 0 */
         sem_post(&customers); /* wake up barber if necessary */
         printf("Recibieno corte de cabello, comensal id: %d\n", ID); /* be seated and be served */
-        pthread_mutex_lock(&personas);
-        num_personas--;
-        pthread_mutex_unlock(&personas);
 
     } else {
-        num_personas--;
         printf("No recibio corte de cabello, comensal id: %d", ID);
         pthread_mutex_unlock(&lock_seats);/* shop is full; do not wait */
     }
-    //releasing any barbers that are still waiting people
-    if (num_personas == 0){
-        for(int i=0;i<N_Barbers;i++){
-            sem_post(&customers);
-        }
-    }
-    
 }
 
 
@@ -65,6 +53,7 @@ int main(){
     pthread_mutex_init(&personas,NULL);
     sem_init(&customers,0,0);
     sem_init(&barberos,0,0);
+    sem_init(&num_personas,0,N_Persons);
     int i;
     for(i=0;i<N_Barbers;i++){
         pthread_create(&barbers[i],NULL,barber,i);
